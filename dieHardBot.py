15ff76da7789b4667006c15fe4910da60bdab1c3
Kris <krismolendyke+github@gmail.com>
2012-12-17 21:37:35 -0500

    Fix workaround for HipChat self-response bug.

--- dieHardBot.py
+++ dieHardBot.py
@@ -5,10 +5,11 @@
 mentions and respond with a random line spoken by that character in the film.
 """
 
-from errbot.botplugin import BotPlugin
 from errbot import botcmd
+from errbot.botplugin import BotPlugin
 from errbot.utils import get_sender_username
 import config
+import logging
 
 from dieHard import DieHard
 
@@ -44,7 +45,11 @@ class DieHardBot(BotPlugin):
         """Listen for Die Hard mentions and interject random lines from those
         characters who were mentioned.
         """
-        if (mess.getFrom() == config.BOT_IDENTITY) or (get_sender_username(mess) == config.CHATROOM_FN):
+        logging.debug("mess.getFrom().getStripped(): " + mess.getFrom().getStripped())
+        logging.debug("config.BOT_IDENTITY['username']: " + config.BOT_IDENTITY["username"])
+        logging.debug("get_sender_username(mess): " + get_sender_username(mess))
+        logging.debug("config.CHATROOM_FN: " + config.CHATROOM_FN)
+        if (mess.getFrom().getStripped() == config.BOT_IDENTITY["username"]) or (get_sender_username(mess) == config.CHATROOM_FN):
             logging.debug("Ignore a message from myself")
             return False
 
